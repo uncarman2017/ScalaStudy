404ed7a2e39d6ebb5ce5eb88eb2834a5afb1d276
Max Yu <31849893+uncarman2017@users.noreply.github.com>
2018-11-07 17:14:53 +0800

    添加 exercise13

---
+++ Study1/src/main/scala/org/redmaple/scala/study1/exercise13/Test1.scala
@@ -0,0 +1,14 @@
+package org.redmaple.scala.study1.exercise13
+
+/**
+  * @author Max Yu
+  * @version 创建时间：2018/11/7 17:09
+  * 13.1 主要的集合特质
+  */
+object Test1 {
+  val set = (1 to 10).toIterator;
+  while(set.hasNext){
+    set.next()
+  }
+
+}
